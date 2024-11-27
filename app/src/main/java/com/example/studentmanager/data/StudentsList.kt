2338c662ed0ef3728cd558ef824e10bdcbf160ca
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-11-27 18:23:58 +0700

    initial commit

---
+++ app/src/main/java/com/example/studentmanager/data/StudentsList.kt
@@ -0,0 +1,17 @@
+package com.example.studentmanager.data
+
+import com.example.studentmanager.model.Student
+
+val students = mutableListOf(
+    Student("Nguyen Van A",20220000),
+    Student("Nguyen Van B",20220001),
+    Student("Nguyen Van C",20220002),
+    Student("Nguyen Van D",20220003),
+    Student("Nguyen Van E",20220004),
+    Student("Nguyen Van F",20220005),
+    Student("Nguyen Van G",20220006),
+    Student("Nguyen Van H",20220007),
+    Student("Nguyen Van I",20220008),
+    Student("Nguyen Van J",20220009),
+    Student("Nguyen Van K",20220010),
+)
